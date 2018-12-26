ca23e5ac0cef3ef19a0799fbd77e3b9961bd0fe5
Villu Orav <villu.orav@4finance.com>
2018-12-26 17:35:29 +0200

    Another solved

--- problem5.rb
+++ problem5.rb
@@ -1,15 +1,25 @@
+load 'problem3.rb'
+
 # Smallest multiple
 # Problem 5
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-def smallest_multiple(min, max)
-  1.upto((min..max).reduce(:*)).detect do |candidate|
-    min.upto(max).all? do |tester|
-      (candidate % tester).zero?
-    end
+def all_factors(min, max)
+  min.upto(max).map do |i|
+    primes(i)
   end
 end
 
-puts smallest_multiple(1, 20)
+def smallest_even_product(min, max)
+  factors = all_factors(min, max)
+  max_primes = factors.flatten.uniq.map do |prime|
+    (prime**factors).map do |factor|
+      factor.count(prime)
+    end.max
+  end
+  max_primes.reduce(:*)
+end
+
+puts smallest_even_product(1, 20)
