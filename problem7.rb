416ce52e283f44c22523cc3592a16c86b697d002
Villu Orav <villu.orav@4finance.com>
2018-12-26 18:22:36 +0200

    Another solved

---
+++ problem7.rb
@@ -0,0 +1,25 @@
+load 'problem3.rb'
+
+# 10001st prime
+# Problem 7
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+# What is the 10 001st prime number?
+
+def prime?(n)
+  primes(n).size == 1
+end
+
+def nth_prime(n)
+  index = 0
+  x = 1
+  loop do
+    if prime?(x)
+      index += 1
+      return x if index == n
+    end
+    x += 1
+  end
+end
+
+puts nth_prime(10_001)
